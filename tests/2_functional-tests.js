be6400d4cb04c2b0d1e967994f87457c9f4d63db
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-08-04 04:53:21 +0000

    Translation with text and locale fields

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,4 +9,19 @@ let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
 
+  test('Translation with text and locale fields: POST request to /api/translate', function(done) {
+    chai.request(server)
+      .post( '/api/translate' )
+      .send( {
+        "text": "Dr Jones",
+        "locale": "american-to-british"
+      } )
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'text');
+        assert.property(res.body, 'translation');
+        done();
+      });
+  });
+
 });
