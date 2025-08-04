e3c85a5f432f5d1096bf9b377aa5e5e3e69ccb3a
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-08-04 05:02:25 +0000

    Translation with text and invalid locale field

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -24,4 +24,19 @@ suite('Functional Tests', () => {
       });
   });
 
+  test('Translation with text and invalid locale field: POST request to /api/translate', function(done) {
+    chai.request(server)
+      .post( '/api/translate' )
+      .send( {
+        "text": "Dr Jones",
+        "locale": "american-to-american"
+      } )
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'error');
+        assert.include(res.body.error, 'Invalid value for locale field');
+        done();
+      });
+  });
+
 });
